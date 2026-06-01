4232017e198c9002e61a09693c9ee0a39c70dde2
Ashita Serrao <ASH_66782@allcargogati.com>
2026-06-01 14:33:09 +0530

    Giving different psswords to all

--- generate_password.py
+++ generate_password.py
@@ -1,5 +1,17 @@
 import streamlit_authenticator as stauth
  
-hashed_password = stauth.Hasher.hash("Welcome@123")
+passwords = [
+    "ashita@123",
+    "arvin@123",
+    "apu@123",
+    "srinivas@123",
+    "devendra@123",
+    "pramod@123",
+    "chandrasekar@123",
+    "sanjay@123",
+    "dinesh@123",
+    "shailendra@123"
+]
  
-print(hashed_password)
+for password in passwords:
+    print(stauth.Hasher.hash(password))
